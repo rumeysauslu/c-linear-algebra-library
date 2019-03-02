56e486e059ff93c3e6488e51b0179f7139413de8
Rumeysa Nur USLU <nur.uslu@bil.omu.edu.tr>
2019-03-02 18:01:14 +0300

    Fix test file

--- src/test_madd.c
+++ src/test_madd.c
@@ -6,15 +6,37 @@ int main(){
   int matrix[2][2] = {1,2,3,4};
   int matrix1[2][2] = {5,6,7,8};
   int o[2][2];
+  int i, j;
 
   matrix_add(*o, *matrix, *matrix1, 2, 2);
-  printf("%d %d %d %d",o[0][0], o[0][1], o[1][0], o[1][1]);
+  printf("toplama işlemi sonucu oluşan matris:");
+  printf("\n");
+  for(i = 0; i < 2; i++){
+    for(j = 0; j < 2; j++){
+      printf(" %d", o[i][j]);
+    }
+  printf("\n \n");
+  }
 
   matrix_sum(*o, *matrix, *matrix1, 2, 2);
-  printf("%d %d %d %d",o[0][0], o[0][1], o[1][0], o[1][1]);
+  printf("çıkarma işlemi sonucu oluşan matris:");
+  printf("\n");
+  for(i = 0; i < 2; i++){
+    for(j = 0; j < 2; j++){
+      printf(" %d", o[i][j]);
+    }
+  printf("\n \n");
+  }
 
   matrix_mult(*o, *matrix, *matrix1, 2, 2, 2);
-  printf("%d %d %d %d",o[0][0], o[0][1], o[1][0], o[1][1]);
+  printf("çarpma işlemi sonucu oluşan matris:");
+  printf("\n");
+  for(i = 0; i < 2; i++){
+    for(j = 0; j < 2; j++){
+      printf(" %d", o[i][j]);
+    }
+  printf("\n \n");
+  }
   
   return 0;
 }
